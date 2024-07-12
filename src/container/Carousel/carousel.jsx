9ae6c7f5aeead09e442bdaae1aa0715415854ecf
Sai Tarun <71441212+saitarun9999@users.noreply.github.com>
2024-07-12 21:09:27 +0530

    menu

--- src/container/Carousel/carousel.jsx
+++ src/container/Carousel/carousel.jsx
@@ -4,17 +4,9 @@ import images from '../../constants/images';
 import './carousel.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
-function play() {
-  const video = document.querySelector('.responsive-video');
-  if (video) {
-    video.play();
-  }
-}
-
 const carousel = () => (
-  <div id="home" className="video-container " >
-    
-    <video className="responsive-video" onLoadedData={play} autoPlay loop muted playsinline >
+  <div id="home" className="video-container" >
+    <video className="responsive-video" autoPlay loop muted>
         <source src={images.video} type="video/mp4" />
       </video>
   </div>
--- src/container/Menu/menu.jsx
+++ src/container/Menu/menu.jsx
@@ -1,4 +1,126 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import './menu.css';
+// import MenuItems from '../../constants/menu';
+// import subCategories from '../../constants/subcategory';
+// import categories from '../../constants/categories';
+// import images from '../../constants/images';
+
+// const Menu = () => {
+
+
+//   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+//   const [selectedSubCategory, setSelectedSubCategory] = useState(subCategories[selectedCategory]?.[0]);
+//   const [openAccordion, setOpenAccordion] = useState(null);
+
+  
+//   const handleCategoryClick = (category) => {
+//     setSelectedCategory([])
+//     setSelectedCategory(category);
+//     setSelectedSubCategory(subCategories[category]?.[0]);
+//   };
+
+//   const handleSubCategoryClick = (subCategory) => {
+//     setSelectedSubCategory([])
+//     setSelectedSubCategory(subCategory);
+//     setOpenAccordion(openAccordion === subCategory ? null : subCategory);
+//   };
+
+
+//   const filteredItems = MenuItems?.filter((item) => {
+//     return item?.category === selectedCategory && item?.subCategory === selectedSubCategory
+//   })
+
+
+//   return (
+//     <div className='menu-section' id='menu'>
+//       <div className='menu_heading'>
+//         <h4 className='menu-heading-title'>Menu</h4>
+//         <p className='menu-heading-description'>
+//           At Bindaz Biryanis, we take pride in offering a diverse and delectable menu that promises to tantalize your taste buds. Each dish is prepared with the finest ingredients and a blend of aromatic spices, ensuring a delightful culinary experience.
+//         </p>
+//       </div>
+//       <div className='categories'>
+//         {categories.map((category) => (
+//           <button
+//             key={category}
+//             className={`category-btn ${selectedCategory === category ? 'active' : ''}`}
+//             onClick={() => handleCategoryClick(category)}
+//           >
+//             {category.toUpperCase()}
+//           </button>
+//         ))}
+//       </div>
+//       {/*  desktop view starts */}
+//       <div className='desktop-view'>
+//        {subCategories?.length !== 0 && (
+//         <div className='subcategories'>
+//           {subCategories[selectedCategory].map((subCategory) => (
+//             <button
+//               key={subCategory}
+//               className={`subcategory-btn ${selectedSubCategory === subCategory ? 'active' : ''}`}
+//               onClick={() => handleSubCategoryClick(subCategory)}
+//             >
+//               {subCategory.toUpperCase()}
+//             </button>
+//           ))}
+//         </div>
+//       )}
+//       <div className='menu-item-container'>
+//         {selectedCategory && images[selectedCategory] && (
+//           <img src={images[selectedCategory]} alt={selectedCategory} height={400} />
+//         )}
+//         <div className='menu-items'>
+//           {filteredItems.map((item) => (
+//             <div className='menu-item' key={item.name} data-category={item.category}>
+//               <button className="subcategory-btn-menu">
+//                 <div className="item-details">
+//                   <span className="item-name">{item.name}</span>
+
+//                   <span className="item-price">{item.price}</span>
+//                 </div>
+//               </button>
+
+//             </div>
+//           ))}
+//         </div>
+//         </div>
+//         </div>
+//         {/*  desktop view ends */}
+//         <div className='mobile-view'>
+//             <div className='accordion'>
+//           {subCategories[selectedCategory]?.map((subCategory, index) => (
+//             <div key={subCategory} className={`accordion-item subcategory-btn  ${openAccordion === subCategory ? 'active' : ''}`}>
+//               <div className='accordion-header'   onClick={() => handleSubCategoryClick(subCategory)}>
+//                 {subCategory.toUpperCase()}
+//               </div>
+//               <div className='accordion-content'>
+//                 {filteredItems
+//                   .filter((item) => item.subCategory === subCategory)
+//                   .map((item) => (
+//                     <div className='menu-item' key={item.name} data-category={item.category}>
+//                       <button className="subcategory-btn-menu">
+//                         <div className="item-details">
+//                           <span className="item-name">{item.name}</span>
+//                           <span className="item-price">{item.price}</span>
+//                         </div>
+//                       </button>
+//                     </div>
+//                   ))}
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+        
+//         </div>
+
+//       </div>
+//   );
+// };
+
+// export default Menu;
+
+
+import React, { useState, useEffect } from 'react';
 import './menu.css';
 import MenuItems from '../../constants/menu';
 import subCategories from '../../constants/subcategory';
@@ -6,29 +128,27 @@ import categories from '../../constants/categories';
 import images from '../../constants/images';
 
 const Menu = () => {
-
-
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
-  const [selectedSubCategory, setSelectedSubCategory] = useState(subCategories[selectedCategory]?.[0]);
+  const [selectedSubCategory, setSelectedSubCategory] = useState(subCategories[selectedCategory]?.[0] || '');
   const [openAccordion, setOpenAccordion] = useState(null);
 
+  useEffect(() => {
+    setSelectedSubCategory(subCategories[selectedCategory]?.[0] || '');
+  }, [selectedCategory]);
 
   const handleCategoryClick = (category) => {
-    setSelectedCategory([])
     setSelectedCategory(category);
-    setSelectedSubCategory(subCategories[category]?.[0]);
+    setOpenAccordion(null); // Reset accordion when category changes
   };
 
   const handleSubCategoryClick = (subCategory) => {
-    setSelectedSubCategory([])
     setSelectedSubCategory(subCategory);
+    setOpenAccordion(openAccordion === subCategory ? null : subCategory);
   };
 
-
-  const filteredItems = MenuItems?.filter((item) => {
-    return item?.category === selectedCategory && item?.subCategory === selectedSubCategory
-  })
-
+  const filteredItems = MenuItems.filter(
+    (item) => item.category === selectedCategory && (item.subCategory === selectedSubCategory || !selectedSubCategory)
+  );
 
   return (
     <div className='menu-section' id='menu'>
@@ -49,70 +169,84 @@ const Menu = () => {
           </button>
         ))}
       </div>
-      {/*  desktop view starts */}
+      {/* Desktop view starts */}
       <div className='desktop-view'>
-       {subCategories?.length !== 0 && (
-        <div className='subcategories'>
-          {subCategories[selectedCategory].map((subCategory) => (
-            <button
-              key={subCategory}
-              className={`subcategory-btn ${selectedSubCategory === subCategory ? 'active' : ''}`}
-              onClick={() => handleSubCategoryClick(subCategory)}
-            >
-              {subCategory.toUpperCase()}
-            </button>
-          ))}
-        </div>
-      )}
-      <div className='menu-item-container'>
-        {selectedCategory && images[selectedCategory] && (
-          <img src={images[selectedCategory]} alt={selectedCategory} height={400} />
-        )}
-        <div className='menu-items'>
-          {filteredItems.map((item) => (
-            <div className='menu-item' key={item.name} data-category={item.category}>
-              <button className="subcategory-btn-menu">
-                <div className="item-details">
-                  <span className="item-name">{item.name}</span>
-
-                  <span className="item-price">{item.price}</span>
-                </div>
-              </button>
-
-            </div>
-          ))}
-        </div>
-        </div>
-        </div>
-        {/*  desktop view ends */}
-        <div className='mobile-view'>
-            <div className='accordion'>
-          {subCategories[selectedCategory]?.map((subCategory, index) => (
-            <div key={subCategory} className={`accordion-item subcategory-btn  ${openAccordion === subCategory ? 'active' : ''}`}>
-              <div className='accordion-header'   onClick={() => handleSubCategoryClick(subCategory)}>
+        {subCategories[selectedCategory]?.length > 0 && (
+          <div className='subcategories'>
+            {subCategories[selectedCategory].map((subCategory) => (
+              <button
+                key={subCategory}
+                className={`subcategory-btn ${selectedSubCategory === subCategory ? 'active' : ''}`}
+                onClick={() => handleSubCategoryClick(subCategory)}
+              >
                 {subCategory.toUpperCase()}
+              </button>
+            ))}
+          </div>
+        )}
+        <div className='menu-item-container'>
+          {selectedCategory && images[selectedCategory] && (
+            <img src={images[selectedCategory]} alt={selectedCategory} height={400} />
+          )}
+          <div className='menu-items'>
+            {filteredItems.map((item) => (
+              <div className='menu-item' key={item.name} data-category={item.category}>
+                <button className="subcategory-btn-menu">
+                  <div className="item-details">
+                    <span className="item-name">{item.name}</span>
+                    <span className="item-price">{item.price}</span>
+                  </div>
+                </button>
               </div>
-              <div className='accordion-content'>
-                {filteredItems
-                  .filter((item) => item.subCategory === subCategory)
-                  .map((item) => (
-                    <div className='menu-item' key={item.name} data-category={item.category}>
-                      <button className="subcategory-btn-menu">
-                        <div className="item-details">
-                          <span className="item-name">{item.name}</span>
-                          <span className="item-price">{item.price}</span>
-                        </div>
-                      </button>
-                    </div>
-                  ))}
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-        
-        </div>
-
       </div>
+      {/* Desktop view ends */}
+      <div className='mobile-view'>
+        {subCategories[selectedCategory]?.length > 0 ? (
+          <div className='accordion'>
+            {subCategories[selectedCategory]?.map((subCategory) => (
+              <div
+                key={subCategory}
+                className={`accordion-item subcategory-btn ${openAccordion === subCategory ? 'active' : ''}`}
+              >
+                <div className='accordion-header' onClick={() => handleSubCategoryClick(subCategory)}>
+                  {subCategory.toUpperCase()}
+                </div>
+                <div className='accordion-content'>
+                  {filteredItems
+                    .filter((item) => item.subCategory === subCategory)
+                    .map((item) => (
+                      <div className='menu-item' key={item.name} data-category={item.category}>
+                        <button className="subcategory-btn-menu">
+                          <div className="item-details">
+                            <span className="item-name">{item.name}</span>
+                            <span className="item-price">{item.price}</span>
+                          </div>
+                        </button>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className='menu-items'>
+            {filteredItems.map((item) => (
+              <div className='menu-item' key={item.name} data-category={item.category}>
+                <button className="subcategory-btn-menu">
+                  <div className="item-details">
+                    <span className="item-name">{item.name}</span>
+                    <span className="item-price">{item.price}</span>
+                  </div>
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
