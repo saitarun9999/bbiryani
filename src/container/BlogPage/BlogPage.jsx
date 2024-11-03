038b1b9793ac42417b84a721c29bedb7ace9c0dd
Sai Tarun <71441212+saitarun9999@users.noreply.github.com>
2024-11-03 22:04:43 +0530

    bindaz

--- src/container/BlogPage/BlogPage.jsx
+++ src/container/BlogPage/BlogPage.jsx
@@ -4,6 +4,14 @@ import Footer from '../Footer/Footer';
 import BlogData from "../../constants/blogData";
 
 const BlogPage = () => {
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // for smooth scrolling
+    });
+  };
+
   return (
     <div>
       <Navbar />
@@ -15,17 +23,19 @@ const BlogPage = () => {
           <div className="grid gap-x-8 gap-y-12 sm:gap-y-16 md:grid-cols-2 lg:grid-cols-3">
             {BlogData.map((blog, index) => (
               <div className="relative" key={index}>
-                <Link to={`/blog/${blog.id}`} className="block overflow-hidden group rounded-xl shadow-lg">
+                <Link to={`/blog/${blog.id}`}  className="block overflow-hidden group rounded-xl shadow-lg">
                   <img
                     src={blog.imageUrl}
                     className="object-cover w-full h-56 transition-all duration-300 ease-out sm:h-64 group-hover:scale-110"
                     alt={blog.title}
                   />
                 </Link>
-                
+
                 <div className="relative mt-5">
                   <p className="uppercase font-semibold text-xs mb-1 text-[#DA281C]">{blog.date}</p>
-                  <Link to={`/blog/${blog.id}`} className="block mb-3 hover:underline">
+                  <Link to={`/blog/${blog.id}`} onClick={handleScrollToTop
+
+} className="block mb-3 hover:underline">
                     <h2 className="text-2xl font-bold leading-tight tracking-wide text-[#DA281C] transition-colors duration-200 hover:text-[#DA281C] line-clamp-2">
                       {blog.title}
                     </h2>
